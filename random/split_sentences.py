7519f9511049bd7013fd72142bec201a1206da32
Vanderlei <hdelei@gmail.com>
2018-08-28 21:32:43 -0300

    Added random scripts folder

---
+++ random/split_sentences.py
@@ -0,0 +1,15 @@
+
+input_string = 'Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica, permanecendo essencialmente inalterado. Se popularizou na década de 60, quando a Letraset lançou decalques contendo passagens de Lorem Ipsum, e mais recentemente quando passou a ser integrado a softwares de editoração eletrônica como Aldus PageMaker.'
+input_lenght = int(input('Digite o comprimento: '))
+array = input_string.split()
+newarray = []
+temp = ''
+
+for x in array:
+    if len(temp) + len(x) <= input_lenght:
+        temp += ' ' + x
+    else:
+        newarray.append(temp)
+        temp = ' ' + x
+        
+[print(x, len(x)) for x in newarray] 
