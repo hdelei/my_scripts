ee70da578f1b4337d7a0561e53f02cf04b88bb95
Vanderlei <hdelei@gmail.com>
2018-09-29 20:46:30 -0300

    The house schedule pre-release

---
+++ TheHouse/schedule.js
@@ -0,0 +1,34 @@
+
+//Get all divs based in bgcolor
+var rows = $('a').filter(function(){
+    var color = $(this).css("background-color").toLowerCase();
+    return color === "rgb(28, 77, 134)" ;
+});
+
+var eventList = 
+{
+    'aula':[], 
+    'data':[], 
+    'sala':[], 
+    'professor':[],
+    'alunos':[] 
+};
+
+var ex = $.expando + "2";
+
+for(i = 0; i < rows.length; i++){
+    eventList.aula.push(rows[i][ex].fcSeg.event.aula);
+    eventList.data.push(rows[i][ex].fcSeg.event.start._i);
+    eventList.sala.push(rows[i][ex].fcSeg.event.sala);
+    eventList.professor.push(rows[i][ex].fcSeg.event.professor);
+    eventList.alunos.push(rows[i][ex].fcSeg.event.numAlunos);
+
+    var temp = 'Aula: ' + eventList.aula[i] + '\n';
+    temp += 'Data e hora: ' + eventList.data[i] + '\n';
+    temp += 'Sala: ' + eventList.sala[i] + '\n';
+    temp += 'Professor: ' + eventList.professor[i] + '\n';
+    temp += 'Número de alunos: ' + eventList.alunos[i];
+
+    alert(temp);    
+}
+
