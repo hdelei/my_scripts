0f8d4a34dfac5bfb0d2a380cd1e7142b4fea7b32
Vanderlei <hdelei@gmail.com>
2018-09-30 23:32:55 -0300

    Showing data in a modal

--- TheHouse/schedule.js
+++ TheHouse/schedule.js
@@ -1,11 +1,10 @@
 /**
  * @file Get The House Schedule Details
  * @author Vanderlei Mendes 
- * @version 1.0
+ * @version 1.1
  * @description Alert Popup with detailed information from checked classes 
  * 
  */
-
 
 //Get all divs based in bgcolor
 var rows = $('a').filter(function(){
@@ -24,6 +23,12 @@ var eventList =
 
 var ex = $.expando + "2";
 
+$('#myModal').modal();
+$('#modalBody').html('');
+$('#modalTitle').html('The House Schedule Summary');
+$('#botaoModal').hide();
+
+//var qrText = '';
 for(i = 0; i < rows.length; i++){
     eventList.aula.push(rows[i][ex].fcSeg.event.aula);
     eventList.data.push(rows[i][ex].fcSeg.event.start._i);
@@ -31,13 +36,23 @@ for(i = 0; i < rows.length; i++){
     eventList.professor.push(rows[i][ex].fcSeg.event.professor);
     eventList.alunos.push(rows[i][ex].fcSeg.event.numAlunos);
 
-    var temp = 'Aula: ' + eventList.aula[i] + '\n';
-    temp += 'Data: ' + new Date(eventList.data[i]).toLocaleDateString('pt-br');
-    temp += ' Hora: ' + eventList.data[i].substring(11, 16) + '\n'
-    temp += 'Sala: ' + eventList.sala[i] + '\n';
-    temp += 'Professor: ' + eventList.professor[i] + '\n';
-    temp += 'Número de alunos: ' + eventList.alunos[i];
-
-    alert(temp);    
+    var temp = '<strong>Aula</strong>: ' + eventList.aula[i] + '<br>';
+    temp += '<strong>Data</strong>: ' + new Date(eventList.data[i]).toLocaleDateString('pt-br');
+    temp += '<strong>Hora</strong>: ' + eventList.data[i].substring(11, 16) + '<br>'
+    temp += '<strong>Sala</strong>: ' + eventList.sala[i] + '<br>';
+    temp += '<strong>Professor</strong>: ' + eventList.professor[i] + '<br>';
+    temp += '<strong>Número de alunos</strong>: ' + eventList.alunos[i] + '<p>';    
+    
+    $('#modalBody').append(temp);
+    //qrText += temp;
 }
 
+/*QRCode part not implemented yet*/
+
+// $('body').append('<script type="text/javascript" src="http://davidshimjs.github.com/qrcodejs/qrcode.min.js"></script>');
+
+// qrText.replace(/<br>|<p>/g, '\n');
+// qrText.replace(/<strong>|<\/strong>/g, '');
+
+// var qrcode = new QRCode("modalBody");
+// qrcode.makeCode(qrText);
